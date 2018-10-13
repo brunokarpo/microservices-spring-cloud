4b649d7b1c2db14c80c549d6605d6b8f3d25013d
Bruno Nogueira de Oliveira <ti.brunonogueira@gmail.com>
2018-10-13 16:36:38 -0300

    Creating project for lab 1

---
+++ laboratorio-1/src/main/kotlin/nom/brunokarpo/microservices/springcloud/lab1/Lab1Application.kt
@@ -0,0 +1,11 @@
+package nom.brunokarpo.microservices.springcloud.lab1
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Lab1Application
+
+fun main(args: Array<String>) {
+    runApplication<Lab1Application>(*args)
+}
