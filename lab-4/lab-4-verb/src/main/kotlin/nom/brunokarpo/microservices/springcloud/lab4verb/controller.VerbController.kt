191d9c491aad664b9d92fe3bcd2d623ea103ee27
Bruno Nogueira de Oliveira <ti.brunonogueira@gmail.com>
2018-10-14 11:20:29 -0300

    Part 2 - Creating verb application client

--- lab-4/lab-4-verb/src/main/kotlin/nom/brunokarpo/microservices/springcloud/lab4verb/controller.VerbController.kt
+++ lab-4/lab-4-verb/src/main/kotlin/nom/brunokarpo/microservices/springcloud/lab4verb/controller.VerbController.kt
@@ -1,11 +1,11 @@
-package nom.brunokarpo.microservices.springcloud.lab4subject.controller
+package nom.brunokarpo.microservices.springcloud.lab4verb
 
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class SubjectController {
+class VerbController {
 
     @Value("\${words}")
     private lateinit var words: String
@@ -16,4 +16,5 @@ class SubjectController {
         var i: Int = Math.round(Math.random() * (wordArray.size - 1)).toInt()
         return wordArray[i]
     }
+
 }
