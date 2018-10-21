66d3897afc0b13eeb59e3fcfde24613f7bce4a3a
Bruno Nogueira de Oliveira <ti.brunonogueira@gmail.com>
2018-10-20 23:23:03 -0300

    Improving code of word controller

--- lab-docker/lab-4-word/src/main/kotlin/nom/brunokarpo/microservices/docker/lab4/controller/WordController.kt
+++ lab-docker/lab-4-word/src/main/kotlin/nom/brunokarpo/microservices/docker/lab4/controller/WordController.kt
@@ -4,17 +4,17 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import java.util.*
 
 @RestController
 class WordController {
 
     @Value("\${words}")
-    private lateinit var words: String
+    private lateinit var words: Array<String>
 
     @GetMapping
     fun getWord(): ResponseEntity<String> {
-        var wordsArray: List<String> = words.split(",")
-        var i: Int = Math.round(Math.random() * (wordsArray.size - 1) ).toInt()
-        return ResponseEntity.ok(wordsArray.get(i))
+        var word = words.get(Random().nextInt(words.size))
+        return ResponseEntity.ok(word)
     }
 }
