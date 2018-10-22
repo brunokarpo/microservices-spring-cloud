9412a159612e6b7fd4741ace2d50c0fdf239891f
Bruno Nogueira de Oliveira <ti.brunonogueira@gmail.com>
2018-10-22 19:05:53 -0300

    Adding Hystrix monitor

--- lab-7/lab-4-sentence/src/main/kotlin/nom/brunokarpo/microservices/docker/lab4sentence/Lab4SentenceApplication.kt
+++ lab-7/lab-4-sentence/src/main/kotlin/nom/brunokarpo/microservices/docker/lab4sentence/Lab4SentenceApplication.kt
@@ -4,12 +4,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 import org.springframework.cloud.netflix.hystrix.EnableHystrix
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard
 import org.springframework.cloud.openfeign.EnableFeignClients
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
 @EnableHystrix
+@EnableHystrixDashboard
 class Lab4SentenceApplication
 
 fun main(args: Array<String>) {
